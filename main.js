f2d1127ba4a21f9d9cca428efadabf0793019329
Gregory.Johnson <3531op@gmail.com>
2020-12-07 08:17:22 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+document.querySelector(`#submit-button`).addEventListener(`click`, () => {
+  const userInput = document.querySelector(`#emagi-input`).value
+  const userOutput = document.querySelector(`#results`)
+  const radioButton = document.querySelectorAll(`.radio-button`)
+  let checkedValue = ``
+  for (const item of radioButton) {
+    if (item.checked === true) {
+      checkedValue += item.value
+    }
+  }
+  switch (checkedValue) {
+
+    case `encode`:
+      userOutput.innerText = encode(userInput)
+      break;
+
+    case `translate`:
+      userOutput.innerText = translate(userInput)
+      break;
+
+    case `madlib`:
+      userOutput.innerText = madlib(userInput)
+      break;
+
+    case `search`:
+      const newArr = search(userInput)
+      for (const item of newArr) {
+        const newPara = document.createElement("P")
+        newPara.innerText = item.symbol
+        userOutput.appendChild(newPara)
+      }
+      break;
+
+    case `random`:
+      if (userInput.length === 0) {
+        userOutput.innerText = randomElement(emojis).symbol
+      } else if (userInput.length > 0) {
+        userOutput.innerText = randomElement(getCategory(userInput)).symbol
+      } else if (userInput.length > 0)
+        break;
+
+    default:
+      console.log(`Please try again.`)
+  }
+})
